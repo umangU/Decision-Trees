1a2c22c3a8f95de485470d5c27330f89d9c234ef
umangU <72771903+umangU@users.noreply.github.com>
2021-05-12 22:41:22 +1000

    Code Updated

--- bagging.r
+++ bagging.r
@@ -28,13 +28,13 @@ for(i in 1:k)
  # Fitting the Bagging Model
  rf.shells=randomForest(Age~.,data=shells ,subset=train, mtry=3, ntree=25)
  
- #Making predictions for the testing dataset
+ # Making predictions for the testing dataset
  yhat.rf = predict(rf.shells, newdata=shells[-train,])
  
- #Storing the test error for each fold
+ # Storing the test error for each fold
  ts_err[i] <- mean((yhat.rf-shells.test)^2)
  print(paste("Mean Test Error for fold", i, ":", ts_err[i]))
 }
 
-#Printing the overall mean test error
+# Printing the overall mean test error
 print(paste("Overall Mean Test Error:", mean(ts_err)))
