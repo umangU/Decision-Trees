d9ccb77299005c7d2722c0d6705783742d10fde2
umangU <72771903+umangU@users.noreply.github.com>
2021-07-19 16:58:43 +1000

    Code Updated

--- Random-Forest.r
+++ Random-Forest.r
@@ -11,7 +11,7 @@ k=10
 n=floor(nrow(shells)/k)
 i=1
 
-# Creating a vector to store the error values
+#Creating a vector to store the error values
 ts_err=vector(,k)
 
 # Looping through the 10 folds
